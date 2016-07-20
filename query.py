5e3ea6828fec17eb74bab583d7779cd674a2565a
nazar-ivantsiv <nazar.ivantsiv@gmail.com>
2016-07-20 23:21:20 +0300

    Fix search sensitivity.

--- query.py
+++ query.py
@@ -1,35 +1,50 @@
 import os
+import re
 
 import pandas as pd
 # import cv2
 
 class Query(object):
-    DB_PATH = 'actions.csv'  # relative path to CSV file
+    DB_PATH = 'action.csv'  # def. relative path to CSV file
     COLUMNS = ['img', 'act_class']  # columns to read from CSV
     VALUES_COLUMN = 'act_class'  # column that contains Values
     
-    def __init__(self, db_path=DB_PATH):
-        self.df = pd.read_csv(db_path, usecols=self.COLUMNS)
-        self.file_names = []
-        
-    @staticmethod
-    def _parse(query_str, separator=' '):
-        """Parse query str into list."""
-        if isinstance(query_str, str):
-            query_lst = query_str.split(separator)
-        return query_lst
-        
+    def __init__(self, db_path=DB_PATH, sep=';'):
+        self.df = pd.read_csv(db_path, usecols=self.COLUMNS, sep=sep)
+                
     def get(self, query_str):
-        """Process the query.
-        Args:
-            query_str -- string from client's UI
-        Returns:
-            paths -- list of paths
-        """
-        query_lst = self._parse(query_str)
-        print(query_lst)
-        self.file_names = self.df[ self.df[self.VALUES_COLUMN].isin(query_lst) ]
+        """Process the query."""
+        re_pattern = self.make_regex(query_str)
+
+        filtered_idxs = self.df[self.VALUES_COLUMN] \
+                            .str.contains( re_pattern )
+            
+        self.query_result = self.df[ filtered_idxs ]
         
+        return self.query_result
+
+    def make_regex(self, query_str, separator=' '):
+        """Parse query str into regex pattern."""
+        if isinstance(query_str, str):
+            words_lst = self._extract_words(query_str.lower(), separator)
+            
+            # create regex ( r'red|shopping|cart' ) pattern to make a query on Vals column
+            re_pattern = r'\b' + r'\b|\b'.join(words_lst) + r'\b'
+            
+        else:
+            # match nothing
+            re_pattern = r'$^'
+        return re_pattern
+
     def view_result(self):
         """View the query results in fancy way."""
-        print(self.file_names)
+        print(self.query_result)
+        self.query_result.head()
+
+    @staticmethod
+    def _extract_words(s, separator=' '):
+        """Split string by white_spaces and strips all the punctuation marks."""
+        EXCLUDE_CHR = '1234567890!%^&*$()_+=-[]{}|,:;\'\"?'
+        result = map( lambda x: x.strip(EXCLUDE_CHR), s.split(separator) )
+
+        return result
