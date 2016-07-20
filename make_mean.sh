da7a0158cdb023f1e0e49cff6262232c4a979756
gvoigt <gvoigt>
2016-07-20 23:14:56 +0300

    correct pathes

--- make_mean.sh
+++ make_mean.sh
@@ -2,11 +2,11 @@
 # Compute the mean image from the imagenet training lmdb
 # N.B. this is available in data/ilsvrc12
 
-EXAMPLE=/home/sam/photo_taggers/image_data
+LMDB=/home/sam/photo_taggers/image_data/MPIIHumanPose_val_lmdb
 CAFFE_TOOLS=/home/sam/builds/caffe/build/tools
-DATA=/home/sam/photo_taggers/image_data
+DATA=/home/sam/photo_taggers/image_data/MPIIHumanPose_val_lmdb
 
-$CAFFE_TOOLS/compute_image_mean $EXAMPLE \
+$CAFFE_TOOLS/compute_image_mean $LMDB \
   $DATA/imagenet_mean.binaryproto
 
 echo "Done."
