6266180d6aa5686b63d8ecd5c06966c04839e03a
gvoigt <gvoigt>
2016-07-21 11:40:46 +0300

    change to relative pathes -> build/tools caffe directory needs to be added to global path

--- make_mean.sh
+++ make_mean.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
-# Compute the mean image from the imagenet training lmdb
-# N.B. this is available in data/ilsvrc12
+# Compute the mean image from the training lmdb
+# the caffe builds/tools directory needs to be added to the global path so
+# the caffe commands can be executed
 
-LMDB=/home/sam/photo_taggers/image_data/MPIIHumanPose_val_lmdb
+
+LMDB=image_data/MPIIHumanPose_val_lmdb
 CAFFE_TOOLS=/home/sam/builds/caffe/build/tools
-DATA=/home/sam/photo_taggers/image_data/MPIIHumanPose_val_lmdb
+DATA=image_data/MPIIHumanPose_val_lmdb
 
-$CAFFE_TOOLS/compute_image_mean $LMDB \
+compute_image_mean $LMDB \
   $DATA/imagenet_mean.binaryproto
 
 echo "Done."
